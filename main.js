e180fb5499b4808a9333eb826ba08313a7a9af88
Samyeol Son <samyeol0930@gmail.com>
2023-12-26 23:04:00 +0900

    import gsap

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 import '/src/styles/style.css';
+import gsap from 'gsap';
+
+const tl = gsap.timeline();
+
+tl.from('.visual', {
+  opacity: 0,
+  y: 30,
+});
+
+tl.from('h2 > span', {
+  opacity: 0,
+  x: -30,
+  stagger: 0.2,
+});
