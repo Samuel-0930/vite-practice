70d2e5135aed831a89ecb5ed3f174dc88ebd18c9
Samyeol Son <samyeol0930@gmail.com>
2023-12-27 19:24:36 +0900

    login state 기억 기능 추가

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import '/src/styles/style.css';
 import gsap from 'gsap';
+import { insertLast, getStorage, getNode, deleteStorage } from '/src/lib';
+import pb from '/src/api/pocketbase.js';
 
 const tl = gsap.timeline();
 
@@ -13,3 +15,25 @@ tl.from('h2 > span', {
   x: -30,
   stagger: 0.2,
 });
+
+if (localStorage.getItem('auth')) {
+  const { isAuth, user } = await getStorage('auth');
+
+  if (isAuth) {
+    const template = /* html */ `
+    <div class="userName">${user.name}님 반갑습니다 😘</div>
+    <button class="logout" type="button">로그아웃</button>
+    `;
+    insertLast('.container', template);
+  }
+}
+
+const logout = getNode('.logout');
+
+if (logout) {
+  logout.addEventListener('click', () => {
+    pb.authStore.clear();
+    deleteStorage('auth');
+    window.location.reload();
+  });
+}
